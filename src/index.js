93dbfaeaf576b76216f451ab0f4373542aa68962
Jack Wagner <wagjack@umich.edu>
2026-01-12 02:18:21 -0500

    More robust postgres code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const {Pool} = require('pg');
 
 const app = express();
-const db = "imagine a database here";
+const db = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome to Tech Projects!');
+});
+
+app.get('/health', async (req, res) => {
+    try {
+        await db.query('SELECT 1');
+        res.status(200).send('OK');
+    } catch (err) {
+        res.status(500).send('Database connection error');
+    }
 });
 
 const PORT = process.env.PORT || 3000;
